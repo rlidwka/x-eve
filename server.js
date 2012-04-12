8ef9d7970deef76edd6aaaf3e1c9e7c4a83010e3
Marc Rene Arns <arns@stridor.net>
2012-04-12 15:15:31 +0200

    make file server simpler by using node-static

--- server.js
+++ server.js
@@ -1,53 +1,23 @@
 /*
   a simple static file server to serve the browser tests
 */
-var http = require("http"),
-    url = require("url"),
-    path = require("path"),
-    fs = require("fs")
-    port = process.argv[2] || 8888;
 
-http.createServer(function(request, response) {
+var static = require('node-static');
+var url = require('url');
+var port = process.argv[2] || 8888;
+var file = new(static.Server)('./');
 
-  var uri = url.parse(request.url).pathname
-    , filename = path.join(process.cwd(), uri);
+require('http').createServer(function (request, response) {
+  request.addListener('end', function () {
+    var uri = url.parse(request.url).pathname;
 
-  if(uri == '' || uri == '/'){
-    response.writeHead(302, {"Location": "/test"});
-    response.end();
-    return;
-  }
-  
-  path.exists(filename, function(exists) {
-    if(!exists) {
-      response.writeHead(404, {"Content-Type": "text/plain"});
-      response.write("404 Not Found\n");
+    if(uri == '' || uri == '/'){
+      response.writeHead(302, {"Location": "/test"});
       response.end();
       return;
     }
 
-	if (fs.statSync(filename).isDirectory()) filename += '/index.html';
-
-    fs.readFile(filename, "binary", function(err, file) {
-      if(err) {        
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(err + "\n");
-        response.end();
-        return;
-      }
-
-      var contentType = "text/html";
-
-      if(filename.match(/\.js$/))
-        contentType = "application/javascript";
-      if(filename.match(/\.css$/))
-        contentType = "text/css";
-            
-
-      response.writeHead(200, {"Content-Type": contentType});
-      response.write(file, "binary");
-      response.end();
-    });
+    file.serve(request, response);
   });
 }).listen(parseInt(port, 10));
 
