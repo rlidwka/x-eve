4a114b5ad44550ec7f468a25d43672256673b9ce
Alex Kocharin <alex@kocharin.ru>
2013-11-16 22:03:24 +0400

    server fix for streams2

--- server.js
+++ server.js
@@ -8,17 +8,15 @@ var port = process.argv[2] || 8888;
 var file = new(static.Server)('./');
 
 require('http').createServer(function (request, response) {
-  request.addListener('end', function () {
-    var uri = url.parse(request.url).pathname;
+	var uri = url.parse(request.url).pathname;
 
-    if(uri == '' || uri == '/'){
-      response.writeHead(302, {"Location": "/test"});
-      response.end();
-      return;
-    }
+	if (uri == '' || uri == '/') {
+		response.writeHead(302, {"Location": "/test"});
+		response.end();
+		return;
+	}
 
-    file.serve(request, response);
-  });
+	file.serve(request, response);
 }).listen(parseInt(port, 10));
 
 console.log("Serving Browser Tests at\n\thttp://localhost:" + port + "/\nCTRL + C to shutdown");
